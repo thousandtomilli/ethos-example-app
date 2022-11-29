9e01a5ba5891d1d9f0161d769c5dfbc71ec8607d
thousandtomilli <118668647+thousandtomilli@users.noreply.github.com>
2022-11-29 14:33:23 +0000

    style etc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import ExampleIcon from "../icons/ExampleIcon";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+
 function MyApp({ Component, pageProps }: AppProps) {
   const ethosConfiguration = {
     // When testing, use our staging link. When in production you may comment this line out.
@@ -15,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       ethosConfiguration={ethosConfiguration}
       dappName="EthosConnect Example App"
       dappIcon={<ExampleIcon />}
-      connectMessage="Your connect message goes here!"
+      connectMessage="Select or Install Sui Wallet Chome Extension for Best Experience. Refresh after sign-in if no puzzle appears!"
     >
       <Component {...pageProps} />
     </EthosConnectProvider>
