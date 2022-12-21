591a5c6be1fca05619fffb32b37fc009198ddb4d
thousandtomilli <118668647+thousandtomilli@users.noreply.github.com>
2022-12-21 12:16:31 +0000

    redirect

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://suizzle.com',
+        permanent: true,
+      },
+    ]
+  }
 }
